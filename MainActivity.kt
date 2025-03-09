f2fed1a4a9c0a015fc832fd97481bfb3cdfecc08
NiPutuAlvinaPutri <148743224+NiPutuAlvinaPutri@users.noreply.github.com>
2025-03-09 21:05:15 +0800

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.helloapps
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Menghubungkan elemen tampilan dengan variabel
+        val inputField = findViewById<EditText>(R.id.editText)
+        val actionButton = findViewById<Button>(R.id.button)
+        val displayText = findViewById<TextView>(R.id.textView)
+
+        // Menetapkan aksi saat tombol ditekan
+        actionButton.setOnClickListener {
+            val inputText = inputField.text.toString().trim()
+
+            // Menampilkan teks sesuai dengan input pengguna
+            displayText.text = if (inputText.isNotEmpty()) {
+                "Hallo, $inputText"
+            } else {
+                "Hallo,Tanpa Nama!"
+            }
+            displayText.visibility = TextView.VISIBLE // Menampilkan teks setelah tombol ditekan
+        }
+    }
+}
